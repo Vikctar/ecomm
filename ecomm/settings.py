ac33c8d1404a31f3dd456524b126a3141da6e657
vikctar <vikc.steve@gmail.com>
2025-06-09 10:51:01 +0300

    default keys for test

--- ecomm/settings.py
+++ ecomm/settings.py
@@ -83,9 +83,9 @@ WSGI_APPLICATION = 'ecomm.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': config('DB_NAME'),
-        'USER': config('DB_USER'),
-        'PASSWORD': config('DB_PASSWORD'),
+        'NAME': config('DB_NAME', default='test_db'),
+        'USER': config('DB_USER', default='test_user'),
+        'PASSWORD': config('DB_PASSWORD', default='password'),
         'HOST': config('DB_HOST', default='localhost'),
         'PORT': config('DB_PORT', default='5432'),
     }
@@ -122,13 +122,13 @@ AFRICASTALKING_API_KEY = config('ATS_KEY')
 
 # Email Configuration
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = config('MAIL_SERVER')
+EMAIL_HOST = config('MAIL_SERVER', default='smtp.gmail.com')
 EMAIL_PORT = config('MAIL_PORT', default=587, cast=int)
 EMAIL_USE_TLS = config('MAIL_USE_TLS', default=True, cast=bool)
-EMAIL_HOST_USER = config('MAIL_USERNAME')
-EMAIL_HOST_PASSWORD = config('MAIL_PASSWORD')
-EMAIL_FROM = config('MAIL_FROM')
-ADMIN_EMAIL = config('ADMIN_EMAIL')
+EMAIL_HOST_USER = config('MAIL_USERNAME', default='email')
+EMAIL_HOST_PASSWORD = config('MAIL_PASSWORD', default='password')
+EMAIL_FROM = config('MAIL_FROM', default='from_email')
+ADMIN_EMAIL = config('ADMIN_EMAIL', default='admin@localhost')
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
