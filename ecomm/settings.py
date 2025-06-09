0a6fc98908d48c876c35211e4f0527aa533dd394
vikctar <vikc.steve@gmail.com>
2025-06-09 11:29:05 +0300

    default keys for test

--- ecomm/settings.py
+++ ecomm/settings.py
@@ -86,8 +86,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': config('DB_NAME', default='test_db'),
-        'USER': config('DB_USER', default='test_user'),
-        'PASSWORD': config('DB_PASSWORD', default='password'),
+        'USER': config('DB_USER', default='postgres'),
+        'PASSWORD': config('DB_PASSWORD', default='postgres'),
         'HOST': config('DB_HOST', default='localhost'),
         'PORT': config('DB_PORT', default='5432'),
     }
